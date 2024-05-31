ceda0875cebaf645f0c7f7fa0b99a65226d5bb95
Gayduk Alexey <gaayduk@gmail.com>
2024-05-31 22:39:01 +0200

    Update

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI recordText;
+    private void Start()
+    {
+        recordText.SetText("–≈ Œ–ƒ œŒ ‘–¿√¿Ã:  " + PlayerPrefs.GetInt("recordKills"));
+    }
     public void Exit()
     {
         Application.Quit();
